e9fa7afa6f8c58326c05462bb734865b9cb79be8
festusfolarin <maicharles001@gmail.com>
2025-01-17 22:37:53 +0100

    fourth commit

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 export function formatCurrency(amount) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -5,4 +7,7 @@ export function formatCurrency(amount) {
   }).format(amount);
 }
 
-export const serverUrl = "http://localhost:3000/api/v1";
+export const serverUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://ecommerce-glasses.onrender.com/api/v1"
+    : "http://localhost:3000/api/v1";
